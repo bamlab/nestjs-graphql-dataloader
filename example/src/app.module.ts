128c1b92df4643d95068b73d7b9a6605d09f9236
JulienIzz <103206306+JulienIzz@users.noreply.github.com>
2025-01-15 11:12:01 +0100

    Chore/update nest (#1)
    * chore: update sqlite3 to yarn
    
    * chore: update all libs to run clean yarn
    
    * chore: update deps
    
    * chore: remove npm check update
    
    * chore: update apollo gateway
    
    ---------
    
    Co-authored-by: Julien IZZILLO <IZZILLJ1@colas.com>

--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -6,11 +6,14 @@ import { join } from 'path';
 import { AccountModule } from './account/account.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { DataLoaderInterceptor } from '../../src'
 
+export
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: true,
       debug: true,
     }),
@@ -28,7 +31,7 @@ import { DataLoaderInterceptor } from '../../src'
     {
       provide: APP_INTERCEPTOR,
       useClass: DataLoaderInterceptor,
-    }
+    },
   ],
 })
-export class AppModule {}
+class AppModule {}
